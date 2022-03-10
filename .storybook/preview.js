97cfa1de2b5808d7587454d4823e013a43e9f40d
Gustavo Cabral <studiogustavocabral@gmail.com>
2022-03-10 16:59:21 -0300

    storybook decorator global

---
+++ .storybook/preview.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import GlobalStyles from '../src/styles/global'
+
+export const decorators = [(Story) => <> <GlobalStyles/><Story /></>]
---
+++ src/components/Heading/index.tsx
@@ -0,0 +1,13 @@
+import * as S from './styles'
+
+export type HeadingProps = { 
+  children: React.ReactNode
+}
+
+const Heading = ({children}: HeadingProps) => (
+  <S.Wrapper>
+  <h1> {children}</h1>
+  </S.Wrapper>
+)
+
+export default Heading
