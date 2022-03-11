6b36ba8aa135bf9cc0fce487510a76f700df620f
Gustavo Cabral <studiogustavocabral@gmail.com>
2022-03-11 00:15:49 -0300

    button fullwidth

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,50 +1,55 @@
 import styled, { css, DefaultTheme } from 'styled-components'
 import {ButtonProps} from '.'
 
-type WrapperProps = Pick<ButtonProps, 'size'>
+type WrapperProps = Pick<ButtonProps, 'size' | 'fullWidth'>
 
 
 const wrapperModifiers = { 
     small: (theme:DefaultTheme) => css`
-        height: 5.6rem;
+        height: 3.6rem;
         font-size: ${theme.font.sizes.small};
+        padding-left:25px;
+        padding-right: 25px;
 
     `,
     medium: (theme:DefaultTheme)=>css`
-       height: 5.6rem;
+       height: 4.6rem;
         font-size: ${theme.font.sizes.small};
+        padding-left:25px;
+        padding-right: 25px;
     `,
     large: (theme:DefaultTheme) => css`
        height: 5.6rem;
-       font-size: ${theme.font.sizes.small};
+       font-size: ${theme.font.sizes.medium};
+       padding-left:55px;
+       padding-right:55px;
     `,
+    fullWidth: () => css`
+        width: 100%;
+    `
   }
 
 export const Wrapper = styled.button<WrapperProps>`
 
-  ${({theme,size}) => css`
+  ${({theme,size, fullWidth}) => css`
+ // display: flex;
+ //  align-items: center;
+ //  justify-content: center;
 
   border-radius: 5px;
-  
-    background-color: #F43253;
 
-    padding-left:25px;
-    padding-right: 25px ;
-    color: white;
-    cursor: pointer;
-    font-size:16px;
-    border-radius: ;
-
-    display: inline-flex;
-    align-items:  center;
-    overflow: hidden;
+    background: #F43253;
+    color: ${theme.colors.white};
+    border: 0;
+    border-radius: ${theme.border.radius};
+    padding: ${theme.spacings.xxsmall};
 
     &:hover {
         background-color: #E41F41;
     }
 
-
     ${!!size && wrapperModifiers[size](theme)}
+    ${!!fullWidth && wrapperModifiers.fullWidth()}
 
   `}
 
